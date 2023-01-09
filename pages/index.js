90c5776312315d7ffd411c74554a8803d94631de
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-01-09 22:35:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
 import Head from 'next/head';
+import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div className='h-screen'>
+    <div className='h-screen bg-[#f3f3f3] snap-y snap-mandatory overflow-y-scroll 
+    overflow-x-hidden z-0'>
       <Head>
         <title>Josh Ivan</title>
         <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/3242/3242257.png" />
       </Head>
 
-      <div className='bg-[#F5F5F5] h-screen'>
+      <div className=''>
         {/* Header */}
           <Header />
         {/* Hero */}
-        <section>
+        <section id='hero' className='snap-start'>
           <Hero />
+        </section>
+        {/* About */}
+        <section id='about' className='snap-center'>
+          <About />
         </section>
       </div>
     </div>
   )
 }
+
