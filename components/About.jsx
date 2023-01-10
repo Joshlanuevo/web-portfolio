38897d88234adf4351ed669c148c3950a838367c
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-01-10 16:43:25 +0800

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
             opacity: 0
             }}
             transition={{
-            duration: 1.2
+            duration: 1
             }}
             whileInView={{ 
             opacity: 1,
@@ -24,13 +24,25 @@ const About = () => {
             src='https://scontent.fmnl8-1.fna.fbcdn.net/v/t1.6435-9/133765778_1940843839401494_724381874854283481_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=VHdVSadriZYAX-BK4KU&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfAZOTKbB96liSAFSUo3LvmZrKZj9UAoCx2QkM2-y0Z7hg&oe=63E379B3'
             className='mt-40 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[600px] xl:h-[600px]' 
         />
-        <div className='space-y-10 px-0 md:px-10'>
-            <div className="text-4xl font-semibold pt-10">{userData.about.headerOne}</div>
+        <motion.div
+            initial={{
+                x: 200,
+                opacity: 0
+            }}
+            transition={{
+                duration: 1
+            }}
+            whileInView={{ 
+                opacity: 1,
+                x: 0 
+            }}
+            className='space-y-10 px-0 md:px-10'
+        >
+            <div className="text-4xl font-semibold pt-10">{userData.about.header}</div>
             <div>
-                <h3 className="text-3xl font-bold">{userData.about.headerTwo}</h3>
                 <p className='text-gray-500'>{userData.about.description}</p>
             </div>
-        </div>
+        </motion.div>
     </div>
   )
 }
