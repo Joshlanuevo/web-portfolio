bc4bfae05cd21571d5041157ddfe6a5269112af6
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-01-12 10:27:14 +0800

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,11 +4,10 @@ import MernHomeWork from '../public/projects/mern-homework.PNG';
 import FiveJCompany from '../public/projects/5j-company.PNG';
 import Eportfolio from '../public/projects/e-portfolio.PNG';
 import Link from 'next/link';
-import HiOutlineExternalLink from 'react-icons/hi'
 
 const Projects = () => {
   return (
-    <div className='h-screen relative bg-white grid place-items-center'>
+    <div className='relative bg-white grid place-items-center'>
         <h3 className='absolute top-20 m-7 uppercase tracking-[20px] text-gray-500 xl:text-xl'>
             Projects
         </h3>
@@ -18,6 +17,7 @@ const Projects = () => {
                 <Image src={MernHomeWork} alt="hero" className='w-[100%] h-[250px]'/>
                 <div className='card-body flex flex-col gap-2 items-center '>
                     <h1 className='p-2.5 font-bold text-lg'>Mern Homework App</h1>
+                    <p className='description mt-0 pt-0 text-gray-400'>Personal Project</p>
                     <h4 className='p-2.5 mx-5 text-center'>
                         A Web Application Homework todo app with user authentication that you can add, edit and delete your homework task.
                     </h4>
@@ -35,6 +35,7 @@ const Projects = () => {
                 <Image src={FiveJCompany} alt="hero" className='w-[100%] h-[250px]'/>
                 <div className='card-body flex flex-col gap-2 items-center'>
                     <h1 className='p-2.5 font-bold text-lg'>5J Company</h1>
+                    <p className='description mt-0 pt-0 text-gray-400'>School Project</p>
                     <h4 className='p-2.5 mx-5 text-center'>
                         A product prototype for our Subject Technopreneurship.
                     </h4>
@@ -42,7 +43,7 @@ const Projects = () => {
                         Tech used:</span> React with styled component and Firebase
                     </p>
                     <Link href="https://j-company-b7e2d.web.app/" target="_blank">
-                        <button className='pt-5 pb-5 px-10 mt-16 rounded-full bg-[#000000] text-white'>
+                        <button className='pt-5 pb-5 px-10 mt-16 mb-10 rounded-full bg-[#000000] text-white'>
                             View
                         </button> 
                     </Link>
@@ -52,13 +53,14 @@ const Projects = () => {
                 <Image src={Eportfolio} alt="hero" className='w-[100%] h-[250px]'/>
                 <div className='card-body flex flex-col gap-2 items-center '>
                     <h1 className='p-2.5 font-bold text-lg'>E-portfolio</h1>
+                    <p className='description mt-0 pt-0 text-gray-400'>School Project</p>
                     <h4 className='p-2.5 mx-5'>
                         A compilation of seminars and certificates for my subject Seminars & Fieldtrips
                     </h4>
                     <p className='pt-7 px-10 text-center'><span className='font-bold'>
                         Tech used:</span> React, CSS, Firebase</p>
                     <Link href="https://e-portfolio-2022-lanuevo.web.app" target="_blank">
-                        <button className='pt-5 pb-5 px-10 mt-20 rounded-full bg-[#000000] text-white'>
+                        <button className='pt-5 pb-5 px-10 mt-10 mb-10 rounded-full bg-[#000000] text-white'>
                             View
                         </button> 
                     </Link>
