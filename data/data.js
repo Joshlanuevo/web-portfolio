6d26a764b97a9ec573fbfcc0fad4326f95112ff3
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-01-13 12:55:43 +0800

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,7 +1,6 @@
 const userData = {
     name: "Josh Ivan Lanuevo",
     destination: "Web Developer",
-    heroUrl: "/2x2-main.png",
     email: "lanuevo68@gmail.com",
     phone: "+63 921 457 7200",
     address: "68 Agudo Street. Mandaluyong City",
