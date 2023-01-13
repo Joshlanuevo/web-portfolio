2e2b6f83e4ef7f7777f1f583cdb02c3d9cb5984f
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-01-13 12:54:24 +0800

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import Image from 'next/image';
 import userData from '../data/data';
+import AboutImg from '../public/aboutImg.png';
 
 const About = () => {
   return (
@@ -8,9 +9,10 @@ const About = () => {
         <h3 className='absolute top-10 m-7 uppercase tracking-[20px] text-gray-500 xl:text-xl'>
             About
         </h3>
-        <img 
-            src='https://scontent.fmnl8-1.fna.fbcdn.net/v/t1.6435-9/133765778_1940843839401494_724381874854283481_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=174925&_nc_ohc=VHdVSadriZYAX-BK4KU&_nc_ht=scontent.fmnl8-1.fna&oh=00_AfAZOTKbB96liSAFSUo3LvmZrKZj9UAoCx2QkM2-y0Z7hg&oe=63E379B3'
-            className='mt-40 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[600px] xl:h-[600px]' 
+        <Image
+            src={AboutImg}
+            alt="about"
+            className='mt-40 md:mb-0 flex-shrink-0 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[600px] xl:h-[600px]' 
         />
         
         <div className='space-y-10 px-0 md:px-10'>
