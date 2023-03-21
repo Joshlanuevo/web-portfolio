ea3fb1b81f7a90e0483212742c5602211a25a679
Josh Ivan Lanuevo <78228676+Joshlanuevo@users.noreply.github.com>
2023-03-21 13:59:35 +0800

    Update data.js

--- data/data.js
+++ data/data.js
@@ -5,13 +5,12 @@ const userData = {
     phone: "+63 921 457 7200",
     address: "68 Agudo Street. Mandaluyong City",
     about: {
-        header: "Hi! I'm a Web Developer focuses on developing solutions using JavasScript, React and NodeJS/Express",
+        header: "Hi! I'm a Web Developer focuses on developing solutions using JavaScript and React",
         description: [
             `I started my journey in Web Development year 2021, I'm not into programming at that time so it was out of curiosity I googled what programming does and Web Development took my attention since the Web browser is too powerful because you don't need to download anything just to open the application you just go to your web browser and use the application there, you can use it wherever you go you only just need is the internet.`,
             `When I'm not coding, I enjoy watching anime series like My Hero Academia, Demon Slayer, Jujutsu Kaisen, and many more. Also, I enjoy playing online games with my friends, and sometimes when I get bored I sketch or draw what I like, such as anime characters or anything.`
         ],
     },
-    projects: [],
     socialLinks: [],
 }
 
